87223c2bf06c71c30e0d649704ca0f3826d26210
Stephen Keenan <stephenkeenan02@gmail.com>
2020-10-23 18:37:01 -0400

    Second commit, CSS and JS

---
+++ script.js
@@ -0,0 +1,6 @@
+function initPage() {
+    const inputEl = document.getElementById("city-input");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+}
